80b2bfe6deccdc8b560a8b48b4cc12db4ddc96aa
Leandro Silva Ferreira <leandrosilvaferreira@gmail.com>
2012-07-24 18:55:55 -0400

    Import inicial

---
+++ src/PingPong.java
@@ -0,0 +1,12 @@
+
+public class PingPong {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ping Pong...");
+	}
+
+}
